a907511d19a4ca069d841f9bc47badf6ae1f8b33
Martynas Šiškauskas <martynassiskauskas@gmail.com>
2020-04-29 23:54:08 +0300

    New control system

---
+++ Games/inzinerijos-proj/Assets/Script/ConnectionWizard.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.IO.Ports;
+
+public class ConnectionWizard : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI data;
+
+    [SerializeField]
+    TMP_Dropdown jumpSensorSelect;
+    [SerializeField]
+    TMP_Dropdown tiltSensorSelect;
+
+    [SerializeField]
+    TextMeshProUGUI message;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        List<TMP_Dropdown.OptionData> list = new List<TMP_Dropdown.OptionData>();
+        foreach (string item in SerialPort.GetPortNames())
+        {
+            list.Add(new TMP_Dropdown.OptionData(item));
+        }
+        jumpSensorSelect.options = list;
+        tiltSensorSelect.options = list;
+
+        if(ControllerInput.instance.connected)
+        {
+            data.text = ControllerInput.buffer;
+        }
+        else
+        {
+            data.text = "No data...";
+        }
+    }
+    public void Connect()
+    {
+        if (ControllerInput.instance.Connect(jumpSensorSelect.options[jumpSensorSelect.value].text,
+            tiltSensorSelect.options[tiltSensorSelect.value].text))
+        {
+            message.color = Color.white;
+            message.text = "Controllers were connected successufully!";
+        }
+        else
+        {
+            message.color = Color.red;
+            message.text = "Controllers failed to connect!";
+        }
+    }
+
+    void Disconnected()
+    {
+
+    }
+}
---
+++ Games/inzinerijos-proj/Assets/Script/ControllerInput.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+using TMPro;
+
+public class ControllerInput : MonoBehaviour
+{
+    public static bool jump { get; private set; }
+    public static bool tilt { get; private set; }
+    public static bool button { get; private set; }
+
+    public static string buffer { get; private set; }
+
+    SerialPort jumpSensor;
+    SerialPort tiltSensor;
+
+    public bool connected = false;
+
+    public Action onConnectFail;
+    public Action onConnectSuccess;
+    public Action onDisconect;
+
+    public static ControllerInput instance;
+
+    private void Awake()
+    {
+        if(!instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public bool Connect(string port1, string port2)
+    {
+        try
+        {
+            jumpSensor = new SerialPort();
+            tiltSensor = new SerialPort();
+            jumpSensor.BaudRate = 9600;
+            tiltSensor.BaudRate = 9600;
+            jumpSensor.PortName = port1;
+            tiltSensor.PortName = port2;
+            jumpSensor.Open();
+            tiltSensor.Open();
+
+            connected = true;
+            onConnectSuccess?.Invoke();
+            return true;
+        }
+        catch
+        {
+            onConnectFail?.Invoke();
+            return false;
+        }
+    }
+    void UpdateBuffer()
+    {
+        buffer = "";
+        while (jumpSensor.BytesToRead > 0)
+        {
+            buffer += Convert.ToChar(jumpSensor.ReadChar());
+        }
+        while (tiltSensor.BytesToRead > 0)
+        {
+            buffer += Convert.ToChar(tiltSensor.ReadChar());
+        }
+        
+    }
+    void Update()
+    {
+        if(jumpSensor != null && tiltSensor != null && jumpSensor.IsOpen && tiltSensor.IsOpen)
+        {
+            UpdateBuffer();
+        }
+        else if(connected)
+        {
+            connected = false;
+            onDisconect?.Invoke();
+        }
+    }
+}
